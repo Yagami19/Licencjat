e90cd9b7a299bf586e36b5587fd136857056a686
Yagami19 <itharil19@gmail.com>
2020-03-30 00:00:05 +0200

    DualPortals
    Copied both portals and made them work to connect two rooms on a separate scene.

--- Licencjat_01/Assets/Scripts/ScreenSizeSetup.cs
+++ Licencjat_01/Assets/Scripts/ScreenSizeSetup.cs
@@ -10,6 +10,16 @@ public class ScreenSizeSetup : MonoBehaviour
     public Camera cameraRed;
     public Material cameraMatRed;
 
+
+
+    public Camera cameraRed2;
+    public Material cameraMatRed2;
+
+
+    public Camera cameraYellow2;
+    public Material cameraMatYellow2;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -39,11 +49,34 @@ public class ScreenSizeSetup : MonoBehaviour
         cameraMatRed.mainTexture = cameraRed.targetTexture;
 
 
+        //
+        if (cameraRed2.targetTexture != null)
+        {
+            cameraRed2.targetTexture.Release();
 
 
+        }
+
+        cameraRed2.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatRed2.mainTexture = cameraRed2.targetTexture;
+        //
+
+        if (cameraYellow2.targetTexture != null)
+        {
+            cameraYellow2.targetTexture.Release();
 
 
+        }
+
+        cameraYellow2.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatYellow2.mainTexture = cameraYellow2.targetTexture;
+
+
+        //
     }
+
+
+
 
 
 
